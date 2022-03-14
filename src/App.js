33dd2c90fcbaace86cbf1b03cff12ef2b9d6c681
prathamesh <prathamesh.khadkban@gmail.com>
2022-03-14 18:33:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Foods from "./Components/Foods";
+import Color from "./Components/Color";
+import Layout from "./Components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        {/* <Color></Color> */}
+        <Foods></Foods>
+      </Layout>
     </div>
   );
 }
