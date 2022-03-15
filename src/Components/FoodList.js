d7434e045bd1698b74f67a763947b1736798d958
prathamesh <prathamesh.khadkban@gmail.com>
2022-03-15 20:01:38 +0530

    remove uncessary part and resolve api issue by make condition ternary op for nutrition value

--- src/Components/FoodList.js
+++ src/Components/FoodList.js
@@ -2,35 +2,21 @@ import Card from "./Card";
 import classes from "./FoodList.module.css";
 
 function FoodList({ item, index }) {
+  console.log(item);
   return (
     <>
       {item.type === "single recipe" && (
-        // <li key={index} className="py-10 flex">
-        //   <img
-        //     className="h-100 w-100 rounded-full"
-        //     src={item.display.images}
-        //     alt=""
-        //   />
-        //   <div className="ml-12">
-        //     <p className="text-sm font-medium text-gray-900">
-        //       {item.display.displayName}
-        //     </p>
-        //     <p className="text-sm text-gray-500">
-        //       {item.content.nutrition &&
-        //         `Calories = ${item.content.nutrition.nutritionEstimates[0].display.value}`}
-        //     </p>
-        //   </div>
-        // </li>
-
         <li key={index} className={classes.item}>
           <Card>
             <img className={classes.image} src={item.display.images} alt="" />
             <div className={classes.content}>
               <h3>{item.display.displayName}</h3>
               <p className="text-sm text-gray-500">
-                {item.content.nutrition &&
-                  `Calories = ${item.content.nutrition.nutritionEstimates[0].display.value}`}
+                {item.content.nutrition.nutritionEstimates.length > 0
+                  ? `Calories = ${item.content.nutrition.nutritionEstimates[0].display.value}`
+                  : `Calories = 100`}
               </p>
+              <p>{`--------------------------------`}</p>
             </div>
             <div className={classes.actions}>
               <button>+</button>
