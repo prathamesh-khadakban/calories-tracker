0f64d69b7e42bae91cf8562dc00508a118f44073
prathamesh <prathamesh.khadkban@gmail.com>
2022-03-15 22:36:16 +0530

    build logic for add quantity

--- src/Components/FoodList.js
+++ src/Components/FoodList.js
@@ -1,31 +1,29 @@
 import Card from "./Card";
 import classes from "./FoodList.module.css";
 
-function FoodList({ item, index }) {
-  console.log(item);
+function FoodList({ addItem, item }) {
+  //console.log(item);
+  const addQuantityHandler = () => {
+    return addItem(item);
+  };
   return (
-    <>
-      {item.type === "single recipe" && (
-        <li key={index} className={classes.item}>
-          <Card>
-            <img className={classes.image} src={item.display.images} alt="" />
-            <div className={classes.content}>
-              <h3>{item.display.displayName}</h3>
-              <p className="text-sm text-gray-500">
-                {item.content.nutrition.nutritionEstimates.length > 0
-                  ? `Calories = ${item.content.nutrition.nutritionEstimates[0].display.value}`
-                  : `Calories = 100`}
-              </p>
-              <p>{`--------------------------------`}</p>
-            </div>
-            <div className={classes.actions}>
-              <button>+</button>
-              <button>-</button>
-            </div>
-          </Card>
-        </li>
-      )}
-    </>
+    <li key={item.id} className={classes.item}>
+      <Card>
+        <img className={classes.image} src={item.image} alt="" />
+        <div className={classes.content}>
+          <h3>{item.name}</h3>
+          <p className="text-sm text-gray-500">
+            {`Calories = ${item.calories}`}
+          </p>
+          <p>{`--------------------------------`}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={addQuantityHandler}>+</button>
+          <span>{item.quantity}</span>
+          <button>-</button>
+        </div>
+      </Card>
+    </li>
   );
 }
 
--- src/Components/Foods.js
+++ src/Components/Foods.js
@@ -1,28 +1,57 @@
 import useFetch from "react-fetch-hook";
 import FoodList from "./FoodList";
 import classes from "./Foods.module.css";
+import { useState } from "react";
 
 const Foods = () => {
-  const { isLoading, data, error } = useFetch(
-    "https://yummly2.p.rapidapi.com/feeds/list?limit=10&start=0",
+  // Food API Call
+  // const { isLoading, data, error } = useFetch(
+  //   "https://yummly2.p.rapidapi.com/feeds/list?limit=10&start=0",
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       "x-rapidapi-host": "yummly2.p.rapidapi.com",
+  //       "x-rapidapi-key": "607f8f232bmsh90b8fc6a8a3d93ap104d97jsnb6025bb03d7a",
+  //     },
+  //   }
+  // );
+
+  //instead of api creating our own object and use unspash images
+  const data_obj = [
     {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "yummly2.p.rapidapi.com",
-        "x-rapidapi-key": "607f8f232bmsh90b8fc6a8a3d93ap104d97jsnb6025bb03d7a",
-      },
-    }
-  );
+      id: 1,
+      name: "Salad",
+      calories: 100,
+      image: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd",
+      quantity: 0,
+    },
+    {
+      id: 2,
+      name: "Pizza",
+      calories: 500,
+      image: "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3",
+      quantity: 0,
+    },
+  ];
+  const [data, setData] = useState(data_obj);
+
+  const addItemHandler = (item) => {
+    // check the given id is already present in data object set or not
+    const updatedData = data.map((eachobj) => {
+      return item.id === eachobj.id
+        ? { ...eachobj, quantity: eachobj.quantity + 1 }
+        : eachobj;
+    });
+
+    setData((prev) => updatedData);
+  };
 
   return (
     <div>
-      {isLoading && <h3>Data is Loading</h3>}
-      {error && <h3>There is some issue with fetching Data</h3>}
-      {/* <ul className="divide-y divide-gray-200"> */}
       <ul className={classes.list}>
         {data &&
-          data.feed.map((item, index) => (
-            <FoodList item={item} index={index}></FoodList>
+          data.map((item) => (
+            <FoodList item={item} addItem={addItemHandler}></FoodList>
           ))}
       </ul>
     </div>
